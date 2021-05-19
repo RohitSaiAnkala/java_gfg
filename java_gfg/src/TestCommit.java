0e314523ce29382ca01bedc2c2523e15becef6c2
rohitsai <rohitsai225@gmail.com>
2021-05-19 10:59:51 +0530

    New Change modified

--- java_gfg/src/TestCommit.java
+++ java_gfg/src/TestCommit.java
@@ -1,6 +1,6 @@
 public class TestCommit {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("New Commit");
+        System.out.println("New Commit ");
     }
 }
